44264d9ebfa5cbb5cd46fd88fe586a940189ad9a
NatalicioNasciment <natalicio.eng.soft@gmail.com>
2016-10-27 01:38:27 -0200

    usuarioEntidade

---
+++ src/main/java/br/com/candystore/persistencia/entidades/Usuario.java
@@ -0,0 +1,37 @@
+package br.com.candystore.persistencia.entidades;
+
+public class Usuario {
+	private Integer id;
+	private String nome;
+	private String senha;
+	private String tipo;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+}
