ea73e7fda883db5b2baf1d52cc323dafb60f8964
NatalicioNasciment <natalicio.eng.soft@gmail.com>
2016-10-28 00:23:38 -0200

    testUsuarioDAO : metodo alterar adicionado

--- src/test/java/br/com/candystore/testUsuarioDAO.java
+++ src/test/java/br/com/candystore/testUsuarioDAO.java
@@ -5,16 +5,34 @@ import br.com.candystore.persistencia.jdbc.UsuarioDAO;
 
 public class testUsuarioDAO {
 	public static void main(String[] args) {
-		
-		//criando o usuario
+		alterar();
+	}
+
+	public static void cadastrar() {
+
 		Usuario usuario = new Usuario();
 		usuario.setNome("Natalicio");
-		usuario.setSenha("natalicio");
-		usuario.setTipo("administrador");
-		//cadastrando o usuario no banco
+		usuario.setLogin("licio");
+		usuario.setSenha("licio");
+		usuario.setTipo("Administrador");
+
 		UsuarioDAO usuarioDAO = new UsuarioDAO();
 		usuarioDAO.cadastrar(usuario);
-		
-		System.out.println("Cadastro realizado com sucesso!!!");
+
+		System.out.println("Usuario cadastrado com sucesso!!!");
+	}
+
+	public static void alterar() {
+		Usuario usuario = new Usuario();
+		usuario.setId(2);
+		usuario.setNome("Natalicio");
+		usuario.setLogin("licio");
+		usuario.setSenha("licio");
+		usuario.setTipo("Administrador");
+
+		UsuarioDAO usuarioDAO = new UsuarioDAO();
+		usuarioDAO.alterar(usuario);
+
+		System.out.println("Usuario alterado com sucesso!!!");
 	}
 }
