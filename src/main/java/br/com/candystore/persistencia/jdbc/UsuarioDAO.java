71f6936f769e242790d45b49ab1bbfbc132e6057
NatalicioNasciment <natalicio.eng.soft@gmail.com>
2016-10-28 00:17:20 -0200

    UsuarioDAO: adicionando o metodo alterar[C

--- src/main/java/br/com/candystore/persistencia/jdbc/UsuarioDAO.java
+++ src/main/java/br/com/candystore/persistencia/jdbc/UsuarioDAO.java
@@ -7,23 +7,43 @@ import java.sql.SQLException;
 import br.com.candystore.persistencia.entidades.Usuario;
 
 public class UsuarioDAO {
+
 	private Connection con = conexaoFactory.getConnection();
 
 	public void cadastrar(Usuario usuario) {
-		String sql = "INSERT INTO usuarios (nome, tipo,senha) VALUES(?, ?, ?)";
+		String sql = "INSERT INTO usuarios(nome, login, senha, tipo) VALUES(?, ?, ?, ?)";
 
-		try {
-			PreparedStatement pst = con.prepareStatement(sql);
+		try (PreparedStatement pst = con.prepareStatement(sql)) {
+			
 			pst.setString(1, usuario.getNome());
-			pst.setString(2, usuario.getTipo());
-			pst.setString(3, usuario.getTipo());
-			
-			pst.execute();
+			pst.setString(2, usuario.getLogin());
+			pst.setString(3, usuario.getSenha());
+			pst.setString(4, usuario.getTipo());
+
+			pst.executeUpdate();
 			pst.close();
-			
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
+	
+	public void alterar(Usuario usuario){
+		String sql = "UPDATE usuarios SET nome = ?, login = ?, senha = ?, tipo = ? WHERE id = ?";
 
+		try (PreparedStatement pst = con.prepareStatement(sql)) {
+			
+			pst.setString(1, usuario.getNome());
+			pst.setString(2, usuario.getLogin());
+			pst.setString(3, usuario.getSenha());
+			pst.setString(4, usuario.getTipo());
+			pst.setInt(5, usuario.getId());
+
+			pst.executeUpdate();
+			pst.close();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 }
